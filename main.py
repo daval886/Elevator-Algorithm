fa42ec8eb7af88c00c59c9a4fe101c8e269c3d89
daval886 <55157309+daval886@users.noreply.github.com>
2021-11-23 16:23:30 +0100

    Final commit
    Added comments

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-fMax = 200
-mSequence = []
-fSequence = []
-trackUp = []
-trackDown = []
+fMax = 100  # max number of floors
+mSequence = []  # modified floor sequence for choosing the closest floor to the starting floor
+fSequence = []  # floor sequence
+trackUp = []    # list of floors to stop at on the way up
+trackDown = []  # list of floors to stop at on the way down
 
 n = int(input("Enter the length of the floor sequence: "))
 
 for i in range(0, n):
-    floor = int(input(str(i+1) + ' - Enter the number of the floor you want to stop at: '))
+    floor = int(input(str(i + 1) + ' - Enter the number of the floor you want to stop at: '))
 
     while floor < 0 or floor > fMax:
         floor = int(input("Incorrect floor number! Please, enter the number from the range 0 - 100: "))
@@ -23,11 +23,12 @@ fSequence.sort()
 print('\nThe elevator will stop on these floors: ' + str(fSequence))
 print('The starting point of the elevator is: ' + str(sFloor) + '\n')
 
-def getDirection():
+
+def getDirection():     # gets direction from starting floor and the closest floor to it
     for j in fSequence:
         mSequence.append(j - sFloor)
 
-    if (min(mSequence)+sFloor) == sFloor:
+    if (min(mSequence) + sFloor) == sFloor:
         mSequence.remove(0)
 
     if min(mSequence) > 0:
@@ -37,18 +38,21 @@ def getDirection():
 
     return direction
 
-def goUp():
+
+def goUp():     # prints out all the floors, which will the elevator stop on on the way up
     for j in fSequence:
         if j > sFloor:
             print(j)
 
-def goDown():
+
+def goDown():   # prints out all the floors, which will the elevator stop on on the way down
     for j in fSequence:
         if j < sFloor:
             trackDown.append(j)
             trackDown.sort(reverse=True)
     for k in trackDown:
         print(k)
+
 
 print('Final elevator sequence:')
 
